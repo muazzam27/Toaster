626e668bb0f57d4ab0c425cbe76f55789de26868
muab <exp.mua@dsg.dk>
2019-09-07 20:06:01 +0500

    latest code

---
+++ app/src/main/java/com/sallinggroup/toastapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.sallinggroup.toastapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+//    public fun buttonClick(view:View):Unit{
+//
+//    }
+}
---
+++ toastmessage/src/main/java/com/sallinggroup/toastmessage/ToastMessage.kt
@@ -0,0 +1,13 @@
+package com.sallinggroup.toastmessage
+
+import android.content.Context
+import android.widget.Toast
+
+public class ToastMessage{
+    companion object{
+        fun message(context:Context,message:String):Unit{
+            Toast.makeText(context,message,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
